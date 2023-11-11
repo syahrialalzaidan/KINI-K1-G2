7e6a88342121a075f70d621ba6fc55eca0624c84
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-11-11 23:44:44 +0700

    feat: Add role-based access control

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,34 @@
-import { withAuth } from "next-auth/middleware"
+import { NextRequestWithAuth, withAuth } from "next-auth/middleware"
+import { NextResponse } from "next/server"
 
 export default withAuth(
-    function middleware (req) {
+    function middleware (request: NextRequestWithAuth) {
+        const userRole = request.nextauth.token?.role;
+
+        if (request.nextUrl.pathname.startsWith("/admin")
+            && userRole != "ADMIN") {
+            return NextResponse.rewrite(
+                new URL("/denied", request.url)
+            )
+        }
+        
+        if (request.nextUrl.pathname.startsWith("/cashier")
+            && userRole != "CASHIER") {
+            if (userRole != "ADMIN") {
+                return NextResponse.rewrite(
+                    new URL("/denied", request.url)
+                )
+            }
+        }
+        
+        if (request.nextUrl.pathname.startsWith("/warehouse")
+            && userRole != "WAREHOUSE") {
+                if (userRole != "ADMIN") {
+                    return NextResponse.rewrite(
+                        new URL("/denied", request.url)
+                    )
+                }
+        }
     },
     {
         callbacks: {
@@ -14,3 +41,5 @@ export default withAuth(
         }
     }
 )
+
+export const config = { matcher: ["/admin/:path*", "/cashier/:path*", "/warehouse/:path*"] }
