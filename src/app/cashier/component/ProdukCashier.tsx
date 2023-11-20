7f6bc05ee5b3b97fd5defed2fec60f1bfb39faa6
Ilmagita <ilmagita@gmail.com>
2023-11-20 20:59:04 +0700

    Done with get product API

---
+++ src/app/cashier/component/ProdukCashier.tsx
@@ -0,0 +1,61 @@
+'use client';
+import Keranjang from "@/components/Keranjang"
+import ProductList from '@/components/ProductList'
+import Searchbar from "@/components/Searchbar";
+import { useState } from 'react';
+
+interface CashierPageProps {
+    produk : Array<Object>
+}
+
+interface Product {
+    id : string,
+    jenisBrg : string,   
+    namaBrg : string, 
+    hargaBrg : number,
+    stok : number, 
+    tglTerima : string,
+    tglUpdate : string,
+    penerima : string,
+    image : string 
+}
+
+interface ProductListProps {
+    products: Product[]
+}
+
+export default function ProdukCashier({{products} : ProductListProps, addToCart}) {
+
+    const [cart, setCart] = useState([])
+    const [showCart, setShowCart] = useState(false)
+
+    const addToCart = (data) => {
+        console.log(data)
+        setCart([...cart, {...data, quantity: 1}])
+    }
+
+    return (
+        <div>
+            <div className='flex'>
+                {
+                    products.map((productItem)=>{
+                        console.log("PRODUCT ITEM : ", productItem)
+                        return (
+                            <div id='product' className='w-[168px] min-h-[261px] p-5 mr-10 relative bg-white rounded-lg shadow'>
+                                <img src={productItem.image} className='w-32 justify-center items-center inline-flex'/>
+                                <p className='text-slate-400 text-base font-bold leading-tight mt-[11px] text-center'>{productItem.namaBrg}</p>
+                                <p className='text-black text-xs font-normal leading-[18px] mt-[6px] text-center'>Rp{new Intl.NumberFormat('id-ID', {minimumFractionDigits: 2}).format(productItem.hargaBrg)}</p>
+                                <button className="w-32 h-8 px-3 py-2 rounded border-2 border-slate-400 justify-center items-center gap-1 inline-flex mt-[11px]"
+                                    onClick={() => addToCart(productItem)}>
+                                    <div className="text-slate-400 text-[9px] font-medium leading-[15px]">Tambah ke Keranjang</div>
+                                </button>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+        </div>
+
+    )
+}
--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -1,7 +1,56 @@
-export default function CashierPage() {
+import Keranjang from "@/components/Keranjang"
+import ProductList from '@/components/ProductList'
+import Searchbar from "@/components/Searchbar";
+import ProdukCashier from "./component/ProdukCashier";
+
+const getCatalog = async() => {
+    try {
+        const res = await fetch("http://localhost:3000/api/product", {
+            cache: "no-store",
+        })
+
+        if (!res.ok) {
+            throw new Error("Faile to fetch the catalog")
+        }
+
+        return res.json()
+    } catch (error) {
+        console.log("Error loading catalog: ", error)
+    }
+}
+
+export default async function CashierPage() {
+    const produk_get = await getCatalog()
+
+    // const [product, setProduct] = useState([
+    //     {
+    //         url: 'https://dagingnesia.id/wp-content/uploads/2021/04/Dada-Fillet-1-kg.jpg',
+    //         name: 'Ayam Fillet Dada',
+    //         category: 'Makanan',
+    //         price: 24000
+    //     },
+    //     {
+    //         url: 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//121/MTA-49899469/coca-cola_coca-cola-can-250-ml_full01.jpg',
+    //         name: 'Coca Cola',
+    //         category: 'Minuman',
+    //         price: 12000
+    //     }
+    // ])
+
+    // const [cart, setCart] = useState([])
+    // console.log(cart)
+    // const [showCart, setShowCart] = useState(false)
+
+    // const addToCart = (data) => {
+    //     console.log(data)
+    //     setCart([...cart, {...data, quantity: 1}])
+    // }
+
     return (
-        <div>
-            CASHIERPAGE
-        </div>
+        <>
+            <ProdukCashier products={produk_get} />
+            <Keranjang cart={cart}></Keranjang>
+        </>
+
     )
 }
