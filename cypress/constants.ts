8699a1c47c3775da3692f8afcdd5d814ee05a7ae
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-11-24 09:47:18 +0700

    feat: adding new constant and custom command

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -1,15 +1,19 @@
 
 export const DEFAULT_USER = {
     id: "65535fe3cd5ac9d43b862ef6",
-    username: "dummy1",
-    name: "naufal",
+    username: "admin",
+    name: "admin",
     password: "123",
     role: "ADMIN",
-},
+}
 
 export const NEW_USER = {
     username: "dummy",
     name: "dummy",
     password: "123",
+}
+
+export const DELETE_USER = {
+    id : "65600ca6df0d51714e2eec57"
 }
 
--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -25,13 +25,19 @@
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 //
-// declare global {
-//   namespace Cypress {
-//     interface Chainable {
-//       login(email: string, password: string): Chainable<void>
-//       drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
-//       visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
-//     }
-//   }
-// }
+
+  declare namespace Cypress {
+    interface Chainable {
+      login(email: string, password: string): Chainable<void>
+    //   drag(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+    //   dismiss(subject: string, options?: Partial<TypeOptions>): Chainable<Element>
+    //   visit(originalFn: CommandOriginalFn, url: string, options: Partial<VisitOptions>): Chainable<Element>
+    }
+  }
+
+  Cypress.Commands.add('login', (username : string, password : string) => { 
+    cy.visit("http://localhost:3000");
+    cy.get("[type=username]").type(username);
+    cy.get("[type=password]").type(password);
+    cy.get("[type=submit]").click();
+ })
