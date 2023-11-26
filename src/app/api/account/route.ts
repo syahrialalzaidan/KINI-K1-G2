800d76faf7b2dddba40d4efcad3eb99355a78ba6
naufalmuhammadn <naufalmuhammadmail@gmail.com>
2023-11-26 13:47:08 +0700

    feat: adding post in account api

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -1,5 +1,35 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma"
+import { NextResponse } from "next/server"
+import bcrypt from "bcrypt"
+
+export async function POST(request: Request) {
+    try {
+        const { username, name, password, role } = await request.json()
+
+        if (!username || !name || !password || !role) {
+            return NextResponse.json({error: 'Data tidak lengkap!'}, { status: 400 })
+        }
+        const isUserExisted = await prisma.user.findUnique({
+            where: {
+                username
+            }
+        })
+
+        if (isUserExisted) {
+            return NextResponse.json({ message: 'Username sudah ada!' }, { status: 406 })
+        }
+        
+        const hashedPassword = await bcrypt.hash(password, 5)
+        
+        const user = await prisma.user.create({
+            data: { username, name, password: hashedPassword, role}
+        })
+        
+        return NextResponse.json(user, { status: 201 })
+    } catch (error: any) {
+        return NextResponse.json(error)
+    }
+}
 
 export async function GET() {
     try {
